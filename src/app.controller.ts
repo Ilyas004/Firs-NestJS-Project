256068ce08d3210741e5f6235a7d660f3b739af0
user <user@user-iMac-13.1>
2024-10-31 14:26:26 +0300

    start project

---
+++ src/app.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Redirect, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+import { articles } from './articles';
+import { Article } from './article.model';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+
+  @Get('create')
+  @Render('create-article')
+  getForm(): void {
+    return;
+  }
+  
+  @Post('articles')
+  @Redirect('/', 301)
+  create(@Body() body: any): void {
+    const id = articles.length + 1;
+    const article = new Article(body.title, body.content, id);
+    articles.push(article);
+  }
+
+  @Get()
+  @Render('index')
+  index() {
+    return { articles };
+  }
+
+  @Get(':id')
+  @Render('article')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return articles.find(article => article.id === id)
+  }
+
+
+}
